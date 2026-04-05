cd7baa6394074346af77a912629400efee602ce3
MchedlishviliNikoloz <mchedlishvili.creative@gmail.com>
2026-04-05 23:54:21 +0400

    feat: add product_service.py with get_all_products, get_product_by_id, add_product

---
+++ services/product_service.py
@@ -0,0 +1,35 @@
+from validators.product_validator import validate_product
+
+
+def get_all_products(filters: dict, products: list) -> list:
+    filtered_products = products
+
+    if "search" in filters:
+        filtered_products = [p for p in filtered_products if filters['search'].lower() in p['name'].lower()]
+
+    if "min_price" in filters:
+        filtered_products = [p for p in filtered_products if filters['min_price'] <= p['price']]
+
+    if "max_price" in filters:
+        filtered_products = [p for p in filtered_products if filters['max_price'] >= p['price']]
+
+    if "limit" in filters:
+        filtered_products = filtered_products[:int(filters["limit"])]
+
+    return filtered_products
+
+def get_product_by_id(product_id: int, products: list):
+    for product in products:
+        if product_id == product['id']:
+            return product
+    return None
+
+def add_product(new_data: dict, existing_data: list):
+    product_validate = validate_product(new_data)
+
+    if not product_validate['success']:
+        return product_validate
+
+    new_data['id'] = len(existing_data) + 1
+    existing_data.append(new_data)
+    return product_validate
